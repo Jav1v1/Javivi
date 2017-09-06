225e65d31830e6082ec9b61cac915f8a2278beab
Jav1v1 <31688941+Jav1v1@users.noreply.github.com>
2017-09-06 12:36:41 +0200

    Add files via upload

---
+++ primer-programa/src/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+programa HolaMundo
+ */
+
+/**
+ *
+ * @author josantam
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("HolaMundo");
+        
+    }
+    
+}
